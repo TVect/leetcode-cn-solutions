c8c39b55f4501aa1f2eecabef54b0e0c30a47a01
TVect <chin340823@163.com>
2020-08-23 21:22:53 +0800

    Add: leetcode 234

---
+++ 234.py
@@ -0,0 +1,41 @@
+"""
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def isPalindrome(self, head: ListNode) -> bool:
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
+
+    # O(n) 时间复杂度和 O(1) 空间复杂度 的做法，需要修改链表
+    # e.g. 用快慢指针找到链表中点后，翻转链表前半部或者后半部，再逐个进行比较
