08abb4df7706912259706e018bea941b5a104201
TVect <chin340823@163.com>
2020-09-13 18:16:10 +0800

    Add: leetcode 437

---
+++ 437.py
@@ -0,0 +1,74 @@
+"""
+给定一个二叉树，它的每个结点都存放着一个整数值。
+
+找出路径和等于给定数值的路径总数。
+
+路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+
+示例：
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+返回 3。和等于 8 的路径有:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3.  -3 -> 11
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+
+        def helper(node, target):
+            """以node开始，和为 target 的路径共有几条"""
+            if node is None:
+                return 0
+            return helper(node.left, target - node.val) + helper(node.right, target - node.val) + (node.val == target)
+
+        if root is None:
+            return 0
+        return self.pathSum(root.left, sum) + self.pathSum(root.right, sum) + helper(root, sum)
+
+
+def construct_tree(num_list):
+    all_nodes = [None if num is None else TreeNode(num) for num in num_list]
+    for idx, node in enumerate(all_nodes):
+        if node is not None:
+            left_child_idx = 2 * idx + 1
+            right_child_idx = 2 * idx + 2
+            node.left = all_nodes[left_child_idx] if left_child_idx < len(num_list) else None
+            node.right = all_nodes[right_child_idx] if right_child_idx < len(num_list) else None
+    return all_nodes[0]
+
+
+root = construct_tree([10, 5, -3, 3, 2, None, 11, 3, -2, None, 1])
+_sum = 8
+
+root = construct_tree([1, -2, -3, 1, 3, -2, None, -1])
+_sum = -1
+print(Solution().pathSum(root, _sum))
