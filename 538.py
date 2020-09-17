249148a8f0fc2259662ac937178665261d21208f
TVect <chin340823@163.com>
2020-09-17 22:37:19 +0800

    Add: leetcode 538

---
+++ 538.py
@@ -0,0 +1,61 @@
+"""
+给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+
+例如：
+
+输入: 原始二叉搜索树:
+              5
+            /   \
+           2     13
+
+输出: 转换为累加树:
+             18
+            /   \
+          20     13
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-bst-to-greater-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        addition = 0
+
+        def helper(node):
+            nonlocal addition
+            if node:
+                helper(node.right)
+                addition = node.val = addition + node.val
+                helper(node.left)
+
+        helper(root)
+        return root
+
+    # 线索二叉树
+
+
+def construct_tree(num_list):
+    all_nodes = [None if num is None else TreeNode(num) for num in num_list]
+    for idx, node in enumerate(all_nodes):
+        if node is not None:
+            left_child_idx = 2 * idx + 1
+            right_child_idx = 2 * idx + 2
+            node.left = all_nodes[left_child_idx] if left_child_idx < len(num_list) else None
+            node.right = all_nodes[right_child_idx] if right_child_idx < len(num_list) else None
+    return all_nodes[0]
+
+
+root = construct_tree([5, 2, 13])
+print(Solution().convertBST(root))
