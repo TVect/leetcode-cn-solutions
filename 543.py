678e8548c03f7b197de175f93e032d4abbe6bbe6
TVect <chin340823@163.com>
2020-09-18 22:59:58 +0800

    Add: leetcode 543

---
+++ 543.py
@@ -0,0 +1,60 @@
+"""
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \
+      4   5
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/diameter-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        diameter = 0
+
+        def get_depth(node):
+            """返回以该节点为根节点的子树的深度"""
+            nonlocal diameter
+            if node:
+                left_depth = get_depth(node.left)
+                right_depth = get_depth(node.right)
+                diameter = max(diameter, left_depth + right_depth)
+                return max(left_depth, right_depth) + 1
+            else:
+                return 0
+
+        get_depth(root)
+        return diameter
+
+
+def construct_tree(num_list):
+    all_nodes = [None if num is None else TreeNode(num) for num in num_list]
+    for idx, node in enumerate(all_nodes):
+        if node is not None:
+            left_child_idx = 2 * idx + 1
+            right_child_idx = 2 * idx + 2
+            node.left = all_nodes[left_child_idx] if left_child_idx < len(num_list) else None
+            node.right = all_nodes[right_child_idx] if right_child_idx < len(num_list) else None
+    return all_nodes[0]
+
+
+root = construct_tree([1, 2, 3, 4, 5])
+print(Solution().diameterOfBinaryTree(root))
