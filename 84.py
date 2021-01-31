72364c64887852048b0e2b77d48ebc68b20272c3
TVect <chin340823@163.com>
2021-01-31 10:20:35 +0800

    Update: leetcode 84

--- 84.py
+++ 84.py
@@ -36,20 +36,44 @@ class Solution:
         return max_area
 
     # 单调栈
-    def largestRectangleArea(self, heights: List[int]) -> int:
+    def largestRectangleArea_2(self, heights: List[int]) -> int:
+        # 维护一个单调不减栈，找到 NextSmallerNumber，此时栈中保存的是前面比这个数小的元素
         ascent_stack = []
         heights = [0] + heights + [0]
         max_area = 0
         for idx in range(len(heights)):
             while len(ascent_stack) > 0 and heights[ascent_stack[-1]] > heights[idx]:
                 tmp_idx = ascent_stack.pop(-1)
+                # 可知 stack[-1] 是前面不大于 tmp_idx 的最近的数, idx 是后面不大于 num 的最近的数
                 area = heights[tmp_idx] * (idx - ascent_stack[-1] -1)
                 max_area = max(max_area, area)
             ascent_stack.append(idx)
         return max_area
 
+    # 和前面的单调栈一样，这里更显式的维护了单调递增的栈，只在下一个元素更小时才判断新的 area
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        max_area = 0
+        stack = []
+        heights = [0] + heights + [0]
+        heights_size = len(heights)
+        for idx in range(heights_size):
+            while stack and heights[idx] <= heights[stack[-1]]:
+                num_idx = stack.pop()
+                # 只在下一个元素更小时才判断新的 area
+                if heights[num_idx] != heights[idx]:
+                    max_area = max(max_area, heights[num_idx] * (idx - stack[-1] - 1))
+            stack.append(idx)
+        return max_area
+
 
 heights = [2, 1, 5, 6, 2, 3]
-heights = [1]
-heights = [2, 0, 2]
+# heights = [1]
+# heights = [2, 1, 2]
+heights = [0, 9]
 print(Solution().largestRectangleArea(heights))
+
+
+
+
+
+
