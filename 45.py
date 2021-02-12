99d310ef858eaa75be755144c22fadf9e19e2764
TVect <chin340823@163.com>
2021-02-12 15:27:02 +0800

    Update: leetcode 45

--- 45.py
+++ 45.py
@@ -37,10 +37,36 @@ class Solution:
                     max_range = nums[idx] + idx
         return -1
 
+    def jump_2(self, nums: List[int]) -> int:
+        nums_size = len(nums)
+        # max_range 表示 step 步至多能走到多远
+        step, max_range = 0, 0
+        idx = 0
+        while max_range < nums_size - 1:
+            tmp = 0
+            while idx <= min(max_range, nums_size-1):
+                tmp = max(tmp, nums[idx] + idx)
+                idx += 1
+            step += 1
+            max_range = tmp
+        return step
+
+    # 动态规划
+    def jump_3(self, nums: List[int]) -> int:
+        nums_size = len(nums)
+        # dp[i] 表示从 i 到 最后一个位置至少需要多少步
+        dp = [-1] * (nums_size - 1) + [0]
+        for idx in range(nums_size - 2, -1, -1):
+            dp[idx] = min([1 + dp[jdx] for jdx in range(idx + 1, min(idx + nums[idx] + 1, nums_size)) if dp[jdx] >= 0],
+                          default=-1)
+        return dp[0]
+
+    # 正向查找最大可以到达的位置
     def jump(self, nums: List[int]) -> int:
         nums_size = len(nums)
+        # max_range 表示 step 步至多能走到多远
         step, end, max_range = 0, 0, 0
-        for idx in range(nums_size-1):
+        for idx in range(nums_size - 1):
             max_range = max(max_range, nums[idx] + idx)
             if idx == end:
                 step += 1
@@ -49,4 +75,6 @@ class Solution:
 
 
 nums = [2, 3, 1, 1, 4]
+# nums = [2, 0, 2, 4, 6, 0, 0, 3]
+# nums = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]
 print(Solution().jump(nums))
