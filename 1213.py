90aad48d933da6f79267f26fd8c7c91bc403f199
TVect <chin340823@163.com>
2021-03-28 16:20:56 +0800

    Update: leetcode 1213

--- 1213.py
+++ 1213.py
@@ -12,6 +12,7 @@ Constraints:
     1 <= arr1[i], arr2[i], arr3[i] <= 2000
 """
 from typing import List
+import heapq
 
 
 class Solution:
@@ -32,8 +33,30 @@ class Solution:
                 pt3 += 1
         return rets
 
+    # 变形形式: 处理数组个数不固定的情况
+    def arraysIntersection_extend(self, in_array: List[List[int]]):
+        n_rows, n_cols = len(in_array), len(in_array[0])
+        pts = [[in_array[idx][0], (idx, 0)] for idx in range(n_rows)]
+        tmp_max_val = max(in_array[idx][0] for idx in range(n_rows))
+        heapq.heapify(pts)
+        rets = []
+        while len(pts) == n_rows:
+            item = heapq.heappop(pts)
+            if item[0] == tmp_max_val:
+                rets.append(tmp_max_val)
+            row_id, col_id = item[1]
+            if col_id + 1 < n_cols:
+                heapq.heappush(pts, [in_array[row_id][col_id + 1], (row_id, col_id+1)])
+                tmp_max_val = max(tmp_max_val, in_array[row_id][col_id + 1])
+        return rets
+
 
 arr1 = [1, 2, 3, 4, 5]
 arr2 = [1, 2, 5, 7, 9]
 arr3 = [1, 3, 4, 5, 8]
 print(Solution().arraysIntersection(arr1, arr2, arr3))
+
+in_array = [[1, 2, 3, 4, 5],
+            [1, 2, 5, 7, 9],
+            [1, 3, 4, 5, 8]]
+print(Solution().arraysIntersection_extend(in_array))
