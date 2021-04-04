9a874f2d686f34458154dd53faec312b1ff99790
TVect <chin340823@163.com>
2021-04-04 14:56:28 +0800

    Update: leetcode 476

--- 476.py
+++ 476.py
@@ -26,7 +26,7 @@
 class Solution:
 
     def findComplement(self, num: int) -> int:
-        return num ^ (pow(2, len(bin(num))-2) - 1)
+        return num ^ ((1 << len(bin(num))-2) - 1)
 
 
 num = 5
